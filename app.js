4ac7631f84f86918a7c6e750b0cb3504b3c6f792
Adrien Deprez <adrien.deprez.fr@gmail.com>
2016-11-18 15:21:55 +0100

    Added dependencies and first version of app.js
    Added bodyParser dependence
    Developped first version of the nodejs server

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
 // App configuration file for the server
+
+var pug = require('pug'),
+    express = require('express'),
+    routes = require('./routes'),
+    api = require('./routes/api'),
+    path = require('path');
+    bodyParser = require('body-parser'),
+    urlencodedParser = bodyParser.urlencoded({ extended: false }),
+    app = express();
+
+// more configuration
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('view options', {
+    layout: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: {}
+      });
+  });
+}
+
+// run the server and leak the port + mode
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port + ' on mode ' + app.settings.env);
+});
