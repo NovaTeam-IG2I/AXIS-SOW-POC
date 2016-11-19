9fdb50a37327df8ec96f685521a4fe770b01c086
Adrien Deprez <adrien.deprez.fr@gmail.com>
2016-11-19 14:04:16 +0100

    Added the api/media part in routing
    Added post, get and delete method for the media listing functions

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // import our API routing code and register it with Express
@@ -24,7 +23,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // register the routes handler
--- routes/api.js
+++ routes/api.js
@@ -7,5 +7,22 @@ var router = express.Router();
 
 //  Some implementation....
 
+router.route('/media')
+  .post(function(req,res){
+
+      //TODO create an importation method to add a media to the database
+      res.send({message:"TODO import a media in the database"});
+  })
+  .delete(function(req,res){
+
+      //TODO create an suppression method to delete a media from the database
+      res.send({message:"TODO delete a media from the database"});
+  })
+  .get(function(req,res){
+
+      //TODO create an get method to read all the media of the database
+      res.send({message:"TODO get all the media of the database"});
+  })
+
 // allow us to use this routing configuration in other files as 'router'
 module.exports = router;
