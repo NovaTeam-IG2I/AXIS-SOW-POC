980f0a39ad6b556d8016fb0f902d2eeb12372116
Adrien Deprez <adrien.deprez.fr@gmail.com>
2016-11-19 15:42:11 +0100

    Finished first version of the REST API routing skeleton
    Added /media, /xmp, /list and /load routes

--- routes/api.js
+++ routes/api.js
@@ -7,21 +7,46 @@ var router = express.Router();
 
 //  Some implementation....
 
-router.route('/media')
+router.route('/list')
+  .put(function(req,res){
+
+      //TODO create an deplacement method to deplace a clip to a particular place
+      res.send({message:"TODO deplace a clip to a location"});
+  })
   .post(function(req,res){
 
-      //TODO create an importation method to add a media to the database
-      res.send({message:"TODO import a media in the database"});
+      //TODO create an importation method to add a clip to the database
+      res.send({message:"TODO import a clip in the database"});
   })
   .delete(function(req,res){
 
-      //TODO create an suppression method to delete a media from the database
-      res.send({message:"TODO delete a media from the database"});
+      //TODO create a suppression method to delete a clip from the database
+      res.send({message:"TODO delete a clip from the database"});
   })
   .get(function(req,res){
 
-      //TODO create an get method to read all the media of the database
-      res.send({message:"TODO get all the media of the database"});
+      //TODO create a get method to read all the clip of the database
+      res.send({message:"TODO get all the clip of the database"});
+  })
+
+router.route('/load')
+  .get(function(req,res){
+
+      //TODO create a loading method to get all the tags of the clip tracks
+      res.send({message:"TODO get all the tags of a clip"})
+  })
+
+router.route('/xmp/:id')
+  .get(function(req,res){
+
+      //TODO create a request methode to get the xmp part corresponding to a tag
+      res.send({message:"TODO get the xmp part corresponding to the tag : " + req.params.id})
+  })
+router.route('/media/:id')
+  .get(function(req,res){
+
+      //TODO create a request methode to get the metadata and the xmp file of a clip
+      res.send({message:"TODO get the metadata and the xmp file of the clip : " + req.params.id})
   })
 
 // allow us to use this routing configuration in other files as 'router'
