5c93facf0633df9f26752a5765d63d85b16fa716
Adrien Deprez <adrien.deprez.fr@gmail.com>
2016-11-29 09:39:06 +0100

    Add multer dependence and show the HTTPRequest file

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,16 @@
 
 var express = require('express');
 var router = express.Router();
-
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
 //  Some implementation....
 
 router.route('/import')
-  .post(function(req,res){
+  .post(upload.single('file'),function(req,res){
 
       //TODO create an deplacement method to deplace a clip to a particular place
       req.header("Content-Type", "multipart/form-data");
+      console.log('Request URL:', req.file);
       res.send({message:"TODO deplace a clip to a location"});
   })
 router.route('/list')
