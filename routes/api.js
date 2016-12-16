1fbb23353517d3ec063ac1b00899055188ffd886
Adrien Deprez <adrien.deprez.fr@gmail.com>
2016-12-16 14:40:23 +0100

    Add exemple metadata for production sheet in the api.

--- routes/api.js
+++ routes/api.js
@@ -16,8 +16,6 @@ router.route('/import')
       //TODO create an deplacement method to deplace a clip to a particular place
       req.header("Content-Type", "multipart/form-data");
       console.log('Request URL:', req.file);
-      //res.send({message:"TODO deplace a clip to a location"});
-
       //
       // Send the imported file to the middleware
       //
@@ -29,8 +27,8 @@ router.route('/import')
       var options = {
         hostname: 'localhost',
         port: 3000,
-        path: '/api/list',
-        method: 'POST',
+        path: '/api/productionsheet/test',
+        method: 'GET',
         headers: {
           'Content-Type': 'multipart/form-data',
           'Content-Length': Buffer.byteLength(postData)
@@ -50,7 +48,7 @@ router.route('/import')
       });
 
       request.on('error', (e) => {
-        console.log(`problem with request: ${e.message}`);
+        console.log('problem with request: ${e.message}');
       });
 
       // write data to request body
@@ -58,41 +56,37 @@ router.route('/import')
       request.end();
         })
 
-router.route('/list')
-  .post(function(req,res){
-
-      //TODO create an importation method to add a clip to the database
-      res.send({message:"TODO import a clip in the database"});
-  })
-  .delete(function(req,res){
-
-      //TODO create a suppression method to delete a clip from the database
-      res.send({message:"TODO delete a clip from the database"});
-  })
+router.route('/productionsheet/:id')
   .get(function(req,res){
 
-      //TODO create a get method to read all the clip of the database
-      res.send({message:"TODO get all the clip of the database"});
+      //TODO create a get method to get all production metadata of a media
+      console.log("TODO get all the production metadata of a media : " + req.params.id);
+      var productionData = new Object();
+      productionData.title = "Titanic";
+      productionData.theme1 = "Drama";
+      productionData.theme2 = "Love";
+      productionData.theme3 = "Disaster";
+      productionData.release = 1997;
+      productionData.duration = 194;
+      productionData.country = "USA";
+      productionData.author = "James Cameron";
+      productionData.director = "James Cameron";
+      productionData.society = "20th Century Fox";
+      res.json(productionData);
   })
 
-router.route('/clip/:id')
+router.route('/technicalsheet/:id')
   .get(function(req,res){
 
-      //TODO create a loading method to get all the tags of the clip tracks
-      res.send({message:"TODO get all the tags of a clip : " + req.params.id})
+      //TODO create a get method to get all technical metadata of a media
+      res.send({message:"TODO get all the production metadata of a media : " + req.params.id});
   })
 
-router.route('/xmp/:id')
+router.route('/clipsheet/:id')
   .get(function(req,res){
 
-      //TODO create a request methode to get the xmp part corresponding to a tag
-      res.send({message:"TODO get the xmp part corresponding to the tag : " + req.params.id})
-  })
-router.route('/media/:id')
-  .get(function(req,res){
-
-      //TODO create a request methode to get the metadata and the xmp file of a clip
-      res.send({message:"TODO get the metadata and the xmp file of the clip : " + req.params.id})
+      //TODO create a get method to get all the metadata of a tag
+      res.send({message:"TODO get all the production metadata of a media : " + req.params.id});
   })
 
 // allow us to use this routing configuration in other files as 'router'
