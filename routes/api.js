88926b33b6714fae784c2d0f0675191d2cadbd3d
Adrien Deprez <adrien.deprez.fr@gmail.com>
2016-12-17 18:32:21 +0100

    Add exemple metadata for technical and clip sheet in the API

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,7 @@ router.route('/import')
 
       //TODO create an deplacement method to deplace a clip to a particular place
       req.header("Content-Type", "multipart/form-data");
-      console.log('Request URL:', req.file);
+      //console.log('Request URL:', req.file);
       //
       // Send the imported file to the middleware
       //
@@ -36,8 +36,8 @@ router.route('/import')
       };
 
       var request = http.request(options, (result) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        console.log(`STATUS: ${result.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(result.headers)}`);
         result.setEncoding('utf8');
         result.on('data', (chunk) => {
           console.log(`BODY: ${chunk}`);
@@ -61,6 +61,42 @@ router.route('/productionsheet/:id')
 
       //TODO create a get method to get all production metadata of a media
       console.log("TODO get all the production metadata of a media : " + req.params.id);
+
+      /*var postData = querystring.stringify({
+        'URI' : req.id
+      });
+
+      var options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/api/productionsheet/test',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Content-Length': Buffer.byteLength(postData)
+        }
+      };
+
+      var request = http.request(options, (result) => {
+        console.log(`STATUS: ${result.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(result.headers)}`);
+        result.setEncoding('utf8');
+        result.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        result.on('end', () => {
+          console.log('No more data in response.');
+        });
+      });
+
+      request.on('error', (e) => {
+        console.log('problem with request: ${e.message}');
+      });
+
+      // write data to request body
+      request.write(postData);
+      request.end();*/
+
       var productionData = new Object();
       productionData.title = "Titanic";
       productionData.theme1 = "Drama";
@@ -73,20 +109,108 @@ router.route('/productionsheet/:id')
       productionData.director = "James Cameron";
       productionData.society = "20th Century Fox";
       res.json(productionData);
+
   })
 
 router.route('/technicalsheet/:id')
   .get(function(req,res){
 
       //TODO create a get method to get all technical metadata of a media
-      res.send({message:"TODO get all the production metadata of a media : " + req.params.id});
+      console.log("TODO get all the technical metadata of a media : " + req.params.id);
+
+      /*var postData = querystring.stringify({
+        'URI' : req.id
+      });
+
+      var options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/api/productionsheet/test',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Content-Length': Buffer.byteLength(postData)
+        }
+      };
+
+      var request = http.request(options, (result) => {
+        console.log(`STATUS: ${result.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(result.headers)}`);
+        result.setEncoding('utf8');
+        result.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        result.on('end', () => {
+          console.log('No more data in response.');
+        });
+      });
+
+      request.on('error', (e) => {
+        console.log('problem with request: ${e.message}');
+      });
+
+      // write data to request body
+      request.write(postData);
+      request.end();*/
+
+      var technicalData = new Object();
+      technicalData.camera = "Zenmuse";
+      technicalData.width = "1920";
+      technicalData.height = "1080";
+      technicalData.lens = "X5R";
+      res.json(technicalData);
+
   })
 
 router.route('/clipsheet/:id')
   .get(function(req,res){
 
       //TODO create a get method to get all the metadata of a tag
-      res.send({message:"TODO get all the production metadata of a media : " + req.params.id});
+      console.log("TODO get all the metadata of a tag : " + req.params.id);
+
+      /*var postData = querystring.stringify({
+        'URI' : req.id
+      });
+
+      var options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/api/productionsheet/test',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Content-Length': Buffer.byteLength(postData)
+        }
+      };
+
+      var request = http.request(options, (result) => {
+        console.log(`STATUS: ${result.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(result.headers)}`);
+        result.setEncoding('utf8');
+        result.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        result.on('end', () => {
+          console.log('No more data in response.');
+        });
+      });
+
+      request.on('error', (e) => {
+        console.log('problem with request: ${e.message}');
+      });
+
+      // write data to request body
+      request.write(postData);
+      request.end();*/
+
+      var tagData = new Object();
+      tagData.start = "00:17:56";
+      tagData.end = "00:21:23";
+      tagData.subject = "Discussion between protagonists";
+      tagData.track = "Video";
+      tagData.media = "Titanic";
+      res.json(tagData);
+
   })
 
 // allow us to use this routing configuration in other files as 'router'
